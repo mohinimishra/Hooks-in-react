39ac142f13975845366fe701d734285d7fdbd6d5
mohinimishra <asmyselfmohini@gmail.com>
2020-12-28 12:19:23 +0530

    added a file in which how to use array with useState

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -3,13 +3,15 @@ import './App.css';
 import HookState from './components/useHook'
 import CounterButton from './components/counterButtonUsingPrevState'
 import UseStateWithObject from './components/useState-with-object'
+import Array from './components/useStatewithArray'
 
 function App() {
   return (
     <div className="App">
       {/* <HookState /> */}
       {/* <CounterButton /> */}
-      <UseStateWithObject />
+      {/* <UseStateWithObject /> */}
+      <Array />
     </div>
   );
 }
