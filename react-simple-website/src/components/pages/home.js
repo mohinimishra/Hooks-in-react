387a2d7c9156bcb9f7d6fcd7a1f1b4e55c17582f
mohinimishra <asmyselfmohini@gmail.com>
2021-01-04 15:36:39 +0530

    added files

---
+++ react-simple-website/src/components/pages/home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../../App.css'
+import Cards from '../Cards'
+import HeroSection from '../HeroSection'
+import Footer from '../Footer'
+
+function Home() {
+    return (
+        <>
+            <HeroSection></HeroSection>
+            <Cards></Cards>
+            <Footer></Footer>
+        </>
+    )
+}
+export default Home
---
+++ react-simple-website/src/components/pages/signUp.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../../App.css'
+
+function Signup() {
+    return (
+        <>
+            <h1 className="sign-up ">SIGNUP</h1>
+        </>
+    )
+}
+export default Signup
